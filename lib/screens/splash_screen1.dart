a0f78b7e84c206fd705e334a42205d42b35a04e5
Frientia <muhamadyajidky@gmail.com>
2025-12-05 20:54:53 +0700

    menambahkan scaffold

--- lib/screens/splash_screen1.dart
+++ lib/screens/splash_screen1.dart
@@ -5,6 +5,15 @@ class SplashScreen1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/images/example.png',
+          width: 200,
+          height: 200,
+        ),
+      ),
+    );
   }
 }
