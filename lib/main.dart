fc19b63be1c2321e9b4c0f3e595ed4d0eb564f85
Frientia <muhamadyajidky@gmail.com>
2025-12-05 19:46:55 +0700

    setup awal lib/main

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyProject());
+}
+
+class MyProject extends StatelessWidget {
+  const MyProject({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
