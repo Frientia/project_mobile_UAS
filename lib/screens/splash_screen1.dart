518aa4a2fc900e8da2d4938f59f3dedaea921336
Frientia <muhamadyajidky@gmail.com>
2025-12-06 13:31:56 +0700

    menambahkan Child center dan mengubah body jadi safearea

--- lib/screens/splash_screen1.dart
+++ lib/screens/splash_screen1.dart
@@ -7,94 +7,96 @@ class SplashScreen1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
+      body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/example.png"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              SizedBox(height: 40),
-              Text(
-                "Welcome to MyConcert",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                "Experience the best concerts around you.",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey[700],
-                ),
-              ),
-              SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 20,
-                    height: 20,
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 174, 31, 202),
-                      shape: BoxShape.circle,
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/example.png"),
+                      fit: BoxFit.cover,
                     ),
                   ),
-                  SizedBox(width: 10),
-                  Container(
-                    width: 12,
-                    height: 12,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xFFedede9),
-                    ),
+                ),
+                SizedBox(height: 40),
+                Text(
+                  "Welcome to MyConcert",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
                   ),
-                  SizedBox(width: 10),
-                  Container(
-                    width: 12,
-                    height: 12,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xFFedede9),
-                    ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Experience the best concerts around you.",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[700],
                   ),
-                ],
-              ),
-              Spacer(),
-              Container(
-                  margin: const EdgeInsets.only(left: 40, right: 40),
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 174, 31, 202),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 174, 31, 202),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 12,
+                      height: 12,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xFFedede9),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 12,
+                      height: 12,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xFFedede9),
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Container(
+                    margin: const EdgeInsets.only(left: 40, right: 40),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 174, 31, 202),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        child: const Text(
+                          'Continue',
+                          style: TextStyle(fontSize: 16, color: Colors.white),
                         ),
                       ),
-                      child: const Text(
-                        'Continue',
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      ),
                     ),
                   ),
-                ),
-              SizedBox(height: 70),
-            ],
+                SizedBox(height: 70),
+              ],
+            ),
           ),
         ),
       ),
