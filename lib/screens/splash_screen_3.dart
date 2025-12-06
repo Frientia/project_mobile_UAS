a9184c5529e37538707c0d9e2f93377ce17c79f6
agrahafiz13 <agrahafiz2@gmail.com>
2025-12-06 16:55:51 +0700

    menambahkan text dan row notif page serta penyesuaian warna di splash screen 3

--- lib/screens/splash_screen_3.dart
+++ lib/screens/splash_screen_3.dart
@@ -26,6 +26,52 @@ class SplashScreen3 extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 40),
+                Text(
+                  "Welcome to MyConcert",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),  
+                SizedBox(height: 20),
+                Text(
+                  "Beli Ticket Konser Murah, Di MyConcert Aja!",
+                  style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 12,
+                      height: 12,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 174, 31, 202),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 12,
+                      height: 12,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xFFedede9),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 12,
+                      height: 12,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xFFedede9),
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(),
               ],
             ),
           ),
