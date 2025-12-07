158298cc70e86b411ec168864998cf7558451007
adityaaji03 <adityaajipramono03@gmail.com>
2025-12-07 10:38:13 +0700

    sturktur awal

---
+++ lib/screens/splash_screen2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_uas/screens/splash_screen2.dart';
+import 'package:mobile_uas/screens/splash_screen3.dart';
+
+class splash_screen2 extends StatelessWidget {
+  const splash_screen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
