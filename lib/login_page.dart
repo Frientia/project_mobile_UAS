b9b593c4b23b5c7b93d4aa8d44c7af0578ab5d06
Frientia <muhamadyajidky@gmail.com>
2025-12-07 10:13:33 +0700

    menambahkan icon, title dan subtitle di dalam childern

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,7 +12,25 @@ class LoginPage extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             constraints: const BoxConstraints(maxWidth: 400),
             child: Column(
-              children: [],
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 60),
+                Icon(Icons.lock_outline, size: 100, color: Colors.purple),
+                SizedBox(height: 20),
+                Text(
+                  'Login to MyConcert',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Silahkan Login untuk melanjutkan',
+                  style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+                ),
+              ],
             ),
           ),
         ),
