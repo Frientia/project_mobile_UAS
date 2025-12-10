7d76aa2765fb992365954bc85bc7f7d2fd24e038
Frientia <muhamadyajidky@gmail.com>
2025-12-10 12:16:53 +0700

    menambahkan defedensi firebase_core di Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_uas/firebase_options.dart';
 import 'screens/splash_screen1.dart';
