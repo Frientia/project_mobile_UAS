7461ccdf28a1a91fc955c811abfb9192d9a5264b
Frientia <muhamadyajidky@gmail.com>
2025-12-10 12:19:17 +0700

    menambahkan defedensi firebase_auth di loginpage

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_uas/register_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
