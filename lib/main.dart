91c35b00df0f670c402694e1fc6b53695be20e8f
agrahafiz13 <agrahafiz2@gmail.com>
2025-12-12 21:15:43 +0700

    update main dan menambahkan depedencies supabase dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:mobile_uas/firebase_options.dart';
 import 'screens/splash_screen1.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
   runApp(const MyProject());
 }
 
