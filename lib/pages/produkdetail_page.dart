febf734eecb5549430b1af96f059de07e64e055f
Frientia <muhamadyajidky@gmail.com>
2025-12-17 13:28:28 +0700

    membuat stfl untuk stup awal

---
+++ lib/pages/produkdetail_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyProduk extends StatefulWidget {
+  const MyProduk({super.key});
+
+  @override
+  State<MyProduk> createState() => _MyProdukState();
+}
+
+class _MyProdukState extends State<MyProduk> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+            children: [],
+        ),
+      ),
+    );
+  }
+}
