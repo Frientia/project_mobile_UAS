0cfa3ef60161ccadda7c98083f184d754155124a
Frientia <muhamadyajidky@gmail.com>
2025-12-17 13:41:14 +0700

    menambah container di dalam children

--- lib/pages/produkdetail_page.dart
+++ lib/pages/produkdetail_page.dart
@@ -15,6 +15,18 @@ class _MyProdukState extends State<MyProduk> {
         child: Center(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 220,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/example.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
