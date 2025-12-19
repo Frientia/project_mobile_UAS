7598201f77e43522e2db74a7757f88c7ae2d7a9d
agrahafiz13 <agrahafiz2@gmail.com>
2025-12-19 19:21:44 +0700

    menambahkan basic profile form

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -7,26 +7,66 @@ class MyProfile extends StatefulWidget {
   State<MyProfile> createState() => _MyProfileState();
 }
 
-final _nameController = TextEditingController();
-final _emailController = TextEditingController();
-final _phoneController = TextEditingController();
-final _passwordController = TextEditingController();
-
-@override
-void dispose() {
-  _nameController.dispose();
-  _emailController.dispose();
-  _phoneController.dispose();
-  _passwordController.dispose();
-  super.dispose();
-}
-
 class _MyProfileState extends State<MyProfile> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _phoneController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Informasi Pribadi')),
-      body: const Center(child: Text('Profile Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            _inputField('Nama', Icons.person, _nameController),
+            _inputField('Email', Icons.email, _emailController),
+            _inputField(
+              'Password',
+              Icons.lock,
+              _passwordController,
+              obscure: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //Widget untuk input field
+  Widget _inputField(
+    String label,
+    IconData icon,
+    TextEditingController controller, {
+    bool obscure = false,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: TextField(
+        controller: controller,
+        obscureText: obscure,
+        decoration: InputDecoration(
+          labelText: label,
+          prefixIcon: Icon(icon),
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
     );
   }
 }
