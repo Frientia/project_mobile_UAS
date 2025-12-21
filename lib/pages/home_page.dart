8ca7c60ce818580f14e0fe7cff457ce7018bc659
adityaaji03 <adityaajipramono03@gmail.com>
2025-12-21 22:09:08 +0700

    menambahkan hero banenr

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class Product {
   final String id;
   final String name;
+  final int priceRegular;
+  final int priceVip;
+  final String imageUrl;
 
-  Product({required this.id, required this.name});
+  Product({
+    required this.id,
+    required this.name,
+    required this.priceRegular,
+    required this.priceVip,
+    required this.imageUrl,
+  });
+
+  factory Product.fromMap(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      name: json['name'],
+      priceRegular: json['price_regular'],
+      priceVip: json['price_vip'],
+      imageUrl: json['image_url'] ?? '',
+    );
+  }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Product> products = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadProducts();
+  }
+
+  Future<void> _loadProducts() async {
+    final response = await Supabase.instance.client.from('products').select();
+
+    products = (response as List).map((e) => Product.fromMap(e)).toList();
+
+    setState(() => isLoading = false);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Home Page")));
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home")),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 180,
+              margin: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                image: const DecorationImage(
+                  image: AssetImage("assets/images/example.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
