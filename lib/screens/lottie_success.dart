78190c7abdefe01a8f293f13aff6cf40ebb19138
agrahafiz13 <agrahafiz2@gmail.com>
2025-12-27 11:51:50 +0700

    setup lottie success

---
+++ lib/screens/lottie_success.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MySuccess extends StatefulWidget {
+  const MySuccess({super.key});
+
+  @override
+  State<MySuccess> createState() => _MySuccessState();
+}
+
+class _MySuccessState extends State<MySuccess> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
