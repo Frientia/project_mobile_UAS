e9a233e429e92a4ffab1371504f53050927790da
agrahafiz13 <agrahafiz2@gmail.com>
2026-01-01 14:24:12 +0700

    setup page developer

---
+++ lib/pages/developer_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyDeveloper extends StatefulWidget {
+  const MyDeveloper({super.key});
+
+  @override
+  State<MyDeveloper> createState() => _MyDeveloperState();
+}
+
+class _MyDeveloperState extends State<MyDeveloper> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
