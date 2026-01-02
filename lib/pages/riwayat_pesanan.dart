debad84a332b38d29712a44cd9396a3890138638
adityaaji03 <adityaajipramono03@gmail.com>
2026-01-03 00:18:45 +0700

    setup state and supabase client for riwayat pesanan

--- lib/pages/riwayat_pesanan.dart
+++ lib/pages/riwayat_pesanan.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class MyRiwayat extends StatelessWidget {
+class MyRiwayat extends StatefulWidget {
   final String firebaseUid;
 
   const MyRiwayat({super.key, required this.firebaseUid});
 
   @override
+  State<MyRiwayat> createState() => _MyRiwayatState();
+}
+
+class _MyRiwayatState extends State<MyRiwayat> {
+  bool isLoading = true;
+  List<Map<String, dynamic>> orders = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Riwayat Pesanan")),
-      body: const Center(child: Text("Riwayat Pesanan")),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : const Center(child: Text("Data Loaded")),
     );
   }
 }
