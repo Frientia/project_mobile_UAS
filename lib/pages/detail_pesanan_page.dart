f69478a6ed5a2b747c3ba8190c2dbaf3c516fb5f
adityaaji03 <adityaajipramono03@gmail.com>
2026-01-03 00:38:24 +0700

    buat stateful dan loading state

--- lib/pages/detail_pesanan_page.dart
+++ lib/pages/detail_pesanan_page.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 
-class DetailPesananPage extends StatelessWidget {
+class DetailPesananPage extends StatefulWidget {
   final String orderId;
 
   const DetailPesananPage({super.key, required this.orderId});
 
   @override
+  State<DetailPesananPage> createState() => _DetailPesananPageState();
+}
+
+class _DetailPesananPageState extends State<DetailPesananPage> {
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 1), () {
+      setState(() => isLoading = false);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Detail Pesanan")),
-      body: Center(child: Text("Detail Pesanan ID: $orderId")),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Center(child: Text("Order ID: ${widget.orderId}")),
     );
   }
 }
