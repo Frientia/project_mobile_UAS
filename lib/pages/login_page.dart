0c5dbcfd2caadf2ef9afc4d002ca5e8b8d1222d0
Frientia <muhamadyajidky@gmail.com>
2026-01-03 17:27:39 +0700

    refactor(ui): ubah side agar lebih rapih

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -386,7 +386,7 @@ class _LoginPageState extends State<LoginPage>
                         style: OutlinedButton.styleFrom(
                           padding: EdgeInsets.symmetric(vertical: 14),
                           backgroundColor: Colors.white,
-                          side: BorderSide(color: Colors.grey),
+                          side: BorderSide(color: Colors.grey.shade300),
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(12),
                           ),
