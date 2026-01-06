65066ca8cce191f0776eed350549825d7c350ac3
agrahafiz13 <agrahafiz2@gmail.com>
2026-01-06 23:47:27 +0700

    menambahkan scaffold

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -8,8 +8,18 @@ class MyAbout extends StatefulWidget {
 }
 
 class _MyAboutState extends State<MyAbout> {
+  static const Color primaryColor = Color.fromARGB(255, 113, 50, 202);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xfff3eaff),
+      appBar: AppBar(
+        title: const Text('About'),
+        backgroundColor: primaryColor,
+        centerTitle: true,
+      ),
+      body: const Center(child: Text('About Page')),
+    );
   }
 }
