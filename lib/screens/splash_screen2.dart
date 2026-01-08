f3da676cf1794f6b4bdb45d008df313eaa4a0629
adityaaji03 <adityaajipramono03@gmail.com>
2026-01-08 11:30:35 +0700

    memperbaiki layout splash screen

--- lib/screens/splash_screen2.dart
+++ lib/screens/splash_screen2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_uas/screens/splash_lottie.dart';
 import 'package:mobile_uas/screens/splash_screen2.dart';
 import 'package:mobile_uas/screens/splash_screen3.dart';
 
@@ -10,98 +11,106 @@ class SplashScreen2 extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 200,
-                height: 200,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/02.jpeg"),
-                    fit: BoxFit.cover,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 40, bottom: 20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/02.jpeg"),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 40),
-              Text(
-                "Welcome My Concert",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+                SizedBox(height: 40),
+                Text(
+                  "Selamat mendegarkan musisi favoritmu",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                "YUK LIHAT LANGSUNG PERFOM MUSISI IDOLA MU",
-                style: TextStyle(fontSize: 16, color: Colors.grey[700]),
-              ),
-              SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 12,
-                    height: 12,
-                    decoration: BoxDecoration(
-                      color: Color(0xFFedede9),
-                      shape: BoxShape.circle,
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Container(
-                    width: 12,
-                    height: 12,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: const Color.fromARGB(255, 174, 31, 202),
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Container(
-                    width: 12,
-                    height: 12,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xFFedede9),
-                    ),
-                  ),
-                ],
-              ),
-              Spacer(),
-Container(
-                margin: const EdgeInsets.only(left: 40, right: 40),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SplashScreen3(),
-                        ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 174, 31, 202),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
+                SizedBox(height: 20),
+                Text(
+                  "Tunggu apalagi yuk beli tiket sekarang!!",
+                  style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 12,
+                      height: 12,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFedede9),
+                        shape: BoxShape.circle,
                       ),
                     ),
-                    child: const Text(
-                      'Continue',
-                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 12,
+                      height: 12,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: const Color.fromARGB(255, 113, 50, 202),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 12,
+                      height: 12,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: const Color.fromARGB(255, 196, 123, 228),
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Container(
+                  margin: const EdgeInsets.only(left: 40, right: 40),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Next Pages
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SplashScreen3(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(
+                          255,
+                          113,
+                          50,
+                          202,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: const Text(
+                        'Continue',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 70),
-
-            ],
+                SizedBox(height: 70),
+              ],
+            ),
           ),
         ),
       ),
