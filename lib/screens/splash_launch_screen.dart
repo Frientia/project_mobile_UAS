1a9aac23ccac6203551f88b1361a3598785ad646
Frientia <muhamadyajidky@gmail.com>
2026-01-08 17:10:59 +0700

    menambahkan MyLauncherScreen

---
+++ lib/screens/splash_launch_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyLauncherScreen extends StatefulWidget {
+  const MyLauncherScreen({super.key});
+
+  @override
+  State<MyLauncherScreen> createState() => _MyLauncherScreenState();
+}
+
+class _MyLauncherScreenState extends State<MyLauncherScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
