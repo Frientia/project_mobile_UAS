857a21b4199304480a8b339be7294b12d9c95d18
agrahafiz13 <agrahafiz2@gmail.com>
2026-01-08 12:19:48 +0700

    setup page profile

---
+++ lib/profil_dev/profil_agra.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AgraProfile extends StatefulWidget {
+  const AgraProfile({super.key});
+
+  @override
+  State<AgraProfile> createState() => _AgraProfileState();
+}
+
+class _AgraProfileState extends State<AgraProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
